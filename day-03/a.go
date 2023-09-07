e8d4bcb97ee5b0a82e83b56b7d4865599fed9c8d
dolencd <dejdolenc@gmail.com>
2023-09-07 20:37:47 +0200

    Add day 3

---
+++ day-03/a.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"unicode"
+)
+
+func main(){
+	input,_ := os.Open("input.txt")
+	defer input.Close()
+	sc := bufio.NewScanner(input)
+
+	var totalPriorities = 0
+	for sc.Scan(){
+		var rucksack = sc.Text()
+		rucksackLeft := []rune(rucksack[:len(rucksack)/2])
+		rucksackRight := []rune(rucksack[len(rucksack)/2:])
+		Exit:
+		for _, rucksackLeftItem := range rucksackLeft{
+			for _, rucksackRightItem := range rucksackRight{
+				if rucksackLeftItem != rucksackRightItem {
+					continue
+				}
+
+				itemValue := int(unicode.ToLower(rucksackLeftItem)-96)
+				
+				if (unicode.IsUpper(rucksackLeftItem)) {
+					totalPriorities += 26
+				}
+				totalPriorities += itemValue
+				break Exit
+			}
+		}
+	}
+	fmt.Println(totalPriorities)
+}
