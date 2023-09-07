c8b4e45081f72b991f6d4734c074f5dacbe2f9bf
dolencd <dejdolenc@gmail.com>
2023-09-07 21:22:13 +0200

    Day 4

---
+++ day-04/a.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main(){
+	input,_ := os.Open("input.txt")
+	defer input.Close()
+	sc := bufio.NewScanner(input)
+
+	var fullyOverlappingPairs = 0
+	for sc.Scan(){
+		var s1, e1, s2, e2 int
+		fmt.Sscanf(sc.Text(), "%d-%d,%d-%d", &s1, &e1, &s2, &e2)
+		if (s1 <= s2 && e1 >= e2) || (s2 <= s1 && e2 >= e1) {
+			fullyOverlappingPairs++
+		}
+		
+	}
+	fmt.Println(fullyOverlappingPairs)
+}
