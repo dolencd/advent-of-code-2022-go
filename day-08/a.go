edcb7a5c66d8c9a3e631df2e6e19518fa1fa44bb
dolencd <dejdolenc@gmail.com>
2023-09-13 21:31:31 +0200

    Day 8

---
+++ day-08/a.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+type point struct {
+	x,y int
+}
+
+func main(){
+	input,err := os.Open("./input.txt")
+	if (err != nil) {
+		fmt.Println((err))
+	}
+	defer input.Close()
+	sc := bufio.NewScanner(input)
+	
+
+	var forest [][]rune
+	for sc.Scan(){
+		row := []rune{}
+		for _, tree := range sc.Text(){
+			row = append(row, tree)
+		}
+		forest = append(forest, row)
+	}
+
+	maxX := len(forest)
+	maxY := len(forest[0])
+	isVisible := make(map[point]bool)
+
+	// Rows
+	for x := 0; x < maxX; x++ {
+		maxSoFar := -1
+		for y := 0; y < maxY; y++ {
+			// Left to right
+			currentTreeHeight := int(forest[x][y]) - 48;
+			if (currentTreeHeight > maxSoFar) {
+				isVisible[point{x, y}] = true;
+				maxSoFar = currentTreeHeight
+			}
+		} 
+
+		maxSoFar = -1
+		for y := maxY - 1; y >= 0; y-- {
+			// Right to left
+			currentTreeHeight := int(forest[x][y]) - 48;
+			if (currentTreeHeight > maxSoFar) {
+				isVisible[point{x, y}] = true;
+				maxSoFar = currentTreeHeight
+			}
+		}
+	} 
+
+	for y := 0; y < maxY; y++ {
+		maxSoFar := -1
+		for x := 0; x < maxX; x++ {
+			// Top to bottom
+			currentTreeHeight := int(forest[x][y]) - 48;
+			if (currentTreeHeight > maxSoFar) {
+				isVisible[point{x, y}] = true;
+				maxSoFar = currentTreeHeight
+			}
+		} 
+
+		maxSoFar = -1
+		for x := maxX - 1; x >= 0; x-- {
+			// Bottom to top
+			currentTreeHeight := int(forest[x][y]) - 48;
+			if (currentTreeHeight > maxSoFar) {
+				isVisible[point{x, y}] = true;
+				maxSoFar = currentTreeHeight
+			}
+		}
+	} 
+
+	for x := 0; x < maxX; x++ {
+		for y := 0; y < maxY; y++ {
+			tmp := isVisible[point{x, y}];
+			if tmp {
+				fmt.Print("x")
+			} else {
+				fmt.Print(" ")
+			}
+		}
+		fmt.Print("\n")
+	}
+
+	fmt.Printf("len(isVisible): %v\n", len(isVisible))
+}
---
+++ day-08/b.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+type point struct {
+	x,y int
+}
+
+type forest struct {
+	trees [][]rune;
+}
+
+func (f *forest) viewLeft (cx, cy int) int {
+	ownHeight := f.trees[cx][cy]
+	total := 0
+	for x := cx - 1; x >= 0; x-- {
+		foundHeight := f.trees[x][cy]
+		if (foundHeight > ownHeight) {
+			break;
+		}
+		total ++
+		if foundHeight == ownHeight {
+			break;
+		}
+	}
+	return total
+}
+
+func (f *forest) viewRight (cx, cy int) int {
+	ownHeight := f.trees[cx][cy]
+	total := 0
+	for x := cx + 1; x < len(f.trees); x++ {
+		foundHeight := f.trees[x][cy]
+		if (foundHeight > ownHeight) {
+			break;
+		}
+		total ++
+		if foundHeight == ownHeight {
+			break;
+		}
+	}
+	return total
+}
+
+func (f *forest) viewUp (cx, cy int) int {
+	ownHeight := f.trees[cx][cy]
+	total := 0
+	for y := cy - 1; y >= 0; y-- {
+		foundHeight := f.trees[cx][y]
+		if (foundHeight > ownHeight) {
+			break;
+		}
+		total ++
+		if foundHeight == ownHeight {
+			break;
+		}
+	}
+	return total
+}
+func (f *forest) viewDown (cx, cy int) int {
+	ownHeight := f.trees[cx][cy]
+	total := 0
+	for y := cy + 1; y < len(f.trees[0]); y++ {
+		foundHeight := f.trees[cx][y]
+		if (foundHeight > ownHeight) {
+			break;
+		}
+		total ++
+		if foundHeight == ownHeight {
+			break;
+		}
+	}
+	return total
+}
+
+func (f *forest) width () int {
+	return len(f.trees[0])
+}
+
+func (f *forest) height () int {
+	return len(f.trees)
+}
+
+func createForest(sc *bufio.Scanner) forest {
+	var trees [][]rune
+	for sc.Scan(){
+		row := []rune{}
+		for _, tree := range sc.Text(){
+			row = append(row, tree)
+		}
+		trees = append(trees, row)
+	}
+	return forest { trees }
+}
+
+func main(){
+	input,err := os.Open("./input.txt")
+	if (err != nil) {
+		fmt.Println((err))
+	}
+	defer input.Close()
+	sc := bufio.NewScanner(input)
+	
+
+	forest := createForest(sc)
+
+	maxScore := 0
+	// Rows
+	for x := 0; x < forest.width(); x++ {
+		for y := 0; y < forest.height(); y++ {
+			currentScore := forest.viewDown(x, y) * forest.viewLeft(x, y) * forest.viewRight(x, y) * forest.viewUp(x, y);
+			if (currentScore > maxScore) {
+				maxScore = currentScore
+			}
+		} 
+	}
+
+	fmt.Printf("maxScore: %v\n", maxScore)
+}
